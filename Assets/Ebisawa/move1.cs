7d952170f181093f5ccbe64b8d33cfaa5d942e52
ebisawashun <shun171219@icloud.com>
2024-05-24 16:22:53 +0900

    playerの動きをした。

---
+++ Assets/Ebisawa/move1.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    float speed = 3.0f;
+
+    void Update()
+    {
+
+        // Dキー（右移動）
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+
+        {
+            transform.position += speed * transform.right * Time.deltaTime;
+        }
+
+        // Aキー（左移動）
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+
+        {
+            transform.position -= speed * transform.right * Time.deltaTime;
+        }
+        transform.position = new Vector2(
+     //エリア指定して移動する
+     Mathf.Clamp(transform.position.x, -7.0f, 7.0f),
+     Mathf.Clamp(transform.position.y, -3.35f, 3.35f)
+     );
+    }
+}
