6d88842dce59c346c0397999719a7e9fa9465f4a
okazawa_R <O.riku8863@gmail.com>
2024-05-25 14:15:32 +0900

    タイトルのBGM、SEのコミット

--- Assets/Shinbo/SceneChanger.cs
+++ Assets/Shinbo/SceneChanger.cs
@@ -8,6 +8,10 @@ public class SceneChanger : MonoBehaviour
 
     [SerializeField] private string loadScene;
 
+    public void Timelag()
+    {
+        Invoke("SwitchScene", 1);
+    }
     public void SwitchScene()
     {
         SceneManager.LoadScene(loadScene);
