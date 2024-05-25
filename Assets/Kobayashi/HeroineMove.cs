428f0fdd1e4944774ee4729a9580e9bd7d48288c
kobayashi koutarou <147145211+kobakoGlhf@users.noreply.github.com>
2024-05-25 11:55:11 +0900

    [fix]バグ修正

--- Assets/Kobayashi/HeroineMove.cs
+++ Assets/Kobayashi/HeroineMove.cs
@@ -7,12 +7,17 @@ using UnityEngine;
 public class HeroineMove : MonoBehaviour
 {
     public float _speed = 5f;
+    [SerializeField]
     float _baseSpeed=0.01f;
     float _rondomSpeed=1f;
     float _timer ;
+    [SerializeField]
+    float _moveTimer;
     bool _moveExecution=true;
     int _time = 2;
     public float _speedRangelow = 0.8f, _speedRangeMax=1.2f;
+    [SerializeField]
+    int _inversion = 180;
     [SerializeField]
     GameObject _leftGameObject;
     [SerializeField]
@@ -31,10 +36,13 @@ public class HeroineMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_leftPosition.x >= this.gameObject.transform.position.x) this.gameObject.transform.Rotate(0,180,0);
-        if (_rightPosition.x <= this.gameObject.transform.position.x) this.gameObject.transform.Rotate(0, 180, 0);
-        if (_moveExecution == true) transform.Translate(Vector2.left * _speed * _baseSpeed * _rondomSpeed, Space.Self);
         _timer += Time.deltaTime;
+        _moveTimer = Time.deltaTime;
+        if (_moveTimer >= 0.001f)
+        {
+            Move();
+            _moveTimer=0;
+        }
         if (_timer >= _time)
         {
             _moveExecution = true;
@@ -53,12 +61,19 @@ public class HeroineMove : MonoBehaviour
     {
         int random = 0;
         random = Random.Range(1, 10);
-        if (random <= 4) this.gameObject.transform.Rotate(0, -180, 0);
+        if (random <= 4) this.gameObject.transform.Rotate(0, -1*_inversion, 0);
         else if (random >= 9)
         {
             _timer = 0;
             _moveExecution = false;
         }
-        else this.gameObject.transform.Rotate(0, 180, 0);
+        else this.gameObject.transform.Rotate(0, _inversion, 0);
+    }
+    private void Move()
+    {
+        if (_leftPosition.x >= this.gameObject.transform.position.x) this.gameObject.transform.Rotate(0, _inversion, 0);
+        if (_rightPosition.x <= this.gameObject.transform.position.x) this.gameObject.transform.Rotate(0, _inversion, 0);
+        if (_moveExecution == true) transform.Translate(Vector2.left * _speed * _baseSpeed * _rondomSpeed, Space.Self);
+        _timer += Time.deltaTime;
     }
 }
