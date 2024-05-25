486382eda9f311cdc60ce7a75641e01909df8065
okazawa_R <O.riku8863@gmail.com>
2024-05-25 12:57:22 +0900

    メッセージのコンポーネントのコミット

--- Assets/Shinbo/messeage.cs
+++ Assets/Shinbo/messeage.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class messeage : MonoBehaviour
 {
     float a;
+    public Text _Text;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,25 @@ public class messeage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //20ïbà»â∫Ç»ÇÁ(40ïbà»è„)
+        if (a <= 20) 
+        {
+            _Text.text = ("");
+        }
+        //ÇQÇOïbà»è„&&ÇSÇOïbÇÊÇËè¨Ç≥Ç¢Ç»ÇÁ(40ïbñ¢ñû20ïbà»è„)
+        if (a >= 20 && a < 40)
+        {
+            _Text.text = ("");
+        }
+        //ÇSÇOïbà»è„&&ÇUÇOïbÇÊÇËè¨Ç≥Ç¢Ç»ÇÁ(20ïbñ¢ñû0ïbà»è„)
+        if(a >= 40 && a < 60)
+        {
+            _Text.text = ("");
+        }
+        //ÇUÇOïbà»è„Ç»ÇÁ(0ïbà»â∫)
+        if(a >= 60)
+        {
+            _Text.text = ("");
+        }
     }
 }
